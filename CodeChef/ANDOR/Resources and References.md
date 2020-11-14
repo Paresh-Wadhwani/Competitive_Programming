37db7a6125aab0cadb95567aaeb8ac615fec070c
parresh77 <parresh77@gmail.com>
2020-11-15 00:20:14 +0530

    Added solution and reference links to ANDOR from CodeChef

---
+++ CodeChef/ANDOR/Resources and References.md
@@ -0,0 +1,2 @@
+**Link to Problem:** https://www.codechef.com/LTIME89B/problems/ANDOR  
+**Link to Editorial:** https://discuss.codechef.com/t/andor-editorial/80312
---
+++ CodeChef/ANDOR/Solutions/C++/CodeChef_ANDOR.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cctype>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+#define ll long long
+#define ull unsigned long long
+
+using namespace std;
+
+int main()
+{
+	ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n << ' ' << 0 << endl;
+    }
+
+	return 0;
+}
